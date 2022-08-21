6c5aa370b014f1e026f556b4f697e90c6a88ec08
Niki <nikiewald@hotmail.dk>
2022-08-21 20:14:52 +0200

    Rewrote the TextNode to use the positional block component, and rewrote it to a function component

--- src/Ast/Elements/Blocks/TextNode.tsx
+++ src/Ast/Elements/Blocks/TextNode.tsx
@@ -1,28 +1,25 @@
-import React from "react";
-import {BlockNode, BlockNodeAst} from "../BlockNode";
+import {useState} from 'react';
+import {BlockNodeAst, BlockNodeProps} from "../BlockNode";
+import './TextNode.css';
+import StyleMap from "../../StyleMap";
+import {PositionalBlock} from "./PositionalBlock";
 
 interface TextNodeAst extends BlockNodeAst {
     value: string;
 }
 
-export default class TextNode extends BlockNode<TextNodeAst> {
-    toJson(): object {
-        return {
-            value: this.props.ast.value,
-            ...super.toJson(),
-        };
-    }
+export function TextNode (props: BlockNodeProps<TextNodeAst>) {
+    const style = new StyleMap(props.ast.style ?? {});
+    const [text, setText] = useState(props.ast.value);
 
-    render(): JSX.Element {
-        // TODO: Figure out how to do this the "react way" instead of "dangerouslySetInnerHTML"
-        let text = this.props.ast.value.replaceAll('\n', '<br>');
-
-        return (
+    return (
+        <PositionalBlock position={props.ast.position} zIndex={props.zIndex} gridSize={props.gridSize} editorMode={props.editorMode}>
             <p
                 className="node-text"
-                style={this.getStyleMap()}
-                dangerouslySetInnerHTML={{__html: text}}
+                style={style.getStyleMap()}
+                // TODO: Figure out how to do this the "react way" instead of "dangerouslySetInnerHTML"
+                dangerouslySetInnerHTML={{__html: text.replaceAll('\n', '<br>')}}
             />
-        );
-    }
+        </PositionalBlock>
+    );
 }
