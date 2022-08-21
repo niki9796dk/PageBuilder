0b95ba482b0a8d41fbf0e3502a5310cc6df64fa8
Niki <nikiewald@hotmail.dk>
2022-08-21 21:12:30 +0200

    Rewrote the QuoteNode to the new block format

--- src/Ast/Elements/Blocks/QuoteNode.tsx
+++ src/Ast/Elements/Blocks/QuoteNode.tsx
@@ -1,27 +1,26 @@
-import {BlockNode, BlockNodeAst} from "../BlockNode";
+import {BlockNodeAst, BlockNodeProps} from "../BlockNode";
+import StyleMap from "../../StyleMap";
+import {useState} from "react";
+import {PositionalBlock} from "./PositionalBlock";
 
 interface QuoteNodeAst extends BlockNodeAst {
     quote: string;
     author: string;
 }
 
-export default class QuoteNode extends BlockNode<QuoteNodeAst> {
-    render(): JSX.Element {
-        return (
-            <figure className="node-quote" style={this.getStyleMap()}>
-                <blockquote>{this.props.ast.quote}</blockquote>
+export default function QuoteNode (props: BlockNodeProps<QuoteNodeAst>) {
+    const style = new StyleMap(props.ast.style ?? {});
+    const [quote, setQuote] = useState(props.ast.quote);
+    const [author, setAuthor] = useState(props.ast.author);
+
+    return (
+        <PositionalBlock position={props.ast.position} zIndex={props.zIndex} gridSize={props.gridSize} editorMode={props.editorMode}>
+            <figure className="node-quote" style={style.getStyleMap()}>
+                <blockquote>{quote}</blockquote>
                 <figcaption style={{float: 'right'}}>
-                    &mdash; <cite>{this.props.ast.author}</cite>
+                    &mdash; <cite>{author}</cite>
                 </figcaption>
             </figure>
-        );
-    }
-
-    toJson(): object {
-        return {
-            quote: this.props.ast.quote,
-            author: this.props.ast.author,
-            ...super.toJson(),
-        };
-    }
+        </PositionalBlock>
+    );
 }
