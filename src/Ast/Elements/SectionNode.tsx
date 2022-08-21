163a332e397a969b148fcff6ce6bb51d04c20f6c
Niki <nikiewald@hotmail.dk>
2022-08-21 21:46:00 +0200

    Refactored SectionNode to be a function component

--- src/Ast/Elements/SectionNode.tsx
+++ src/Ast/Elements/SectionNode.tsx
@@ -1,11 +1,10 @@
-import {AST} from "../types";
-import {BlockNode, BlockNodeAst} from "./BlockNode";
 import {BlockFactory} from "../BlockFactory";
 import StyleMap from "../StyleMap";
 import GridNode from "../ElementProperties/GridNode";
-import React from "react";
+import React, {useRef} from "react";
 import {AstNode} from "../../types";
 import _ from "lodash";
+import {BlockNodeAst} from "./Blocks/PositionalBlock";
 
 interface SectionNodeAst {
     type: string;
@@ -14,66 +13,43 @@ interface SectionNodeAst {
     grid: any;
 }
 
-type Blocks = {
-    [key: string]: BlockNode<any>;
-};
+export function SectionNode (props: AstNode<SectionNodeAst>) {
+    const grid = useRef<GridNode | null>(null);
+    const style = new StyleMap(props.ast.style);
 
-export class SectionNode extends React.Component<AstNode<SectionNodeAst>, any> implements AST {
-    public readonly blocks: Blocks = {};
-    public grid: GridNode|null = null
-    public readonly style: StyleMap;
-
-    constructor(props: Readonly<AstNode<SectionNodeAst>> | AstNode<SectionNodeAst>) {
-        super(props);
-
-        this.style = new StyleMap(props.ast.style ?? {});
-    }
-
-    private getSectionHeight(): number {
+    const getSectionHeight = () => {
         return Math.max(..._.map(
-            this.props.ast.blocks,
+            props.ast.blocks,
             (blockNode) => blockNode.position.top + blockNode.position.height + 1),
         );
     }
 
-    toJson(): object {
+    const getGridSize = () => {
         return {
-            type: this.props.ast.type,
-            blocks: _.map(this.blocks, (blockNode) => blockNode.toJson()),
-            style: this.style.toJson(),
-            grid: this.props.ast.grid,
-        };
+            cellWidth: 1200 / 24,
+            cellHeight: 25,
+        }
     }
 
-    render() {
-        return <div className="node-section" style={this.style.getStyleMap()}>
-            <GridNode
-                ast={this.props.ast.grid}
-                editorMode={this.props.editorMode}
-                gridHeight={this.getSectionHeight()}
-                children={this.renderBlocks()}
-                ref={(grid) => this.grid = grid}
-            />
-        </div>
-    }
-
-    private renderBlocks(): JSX.Element[] {
-        return _.map(this.props.ast.blocks, (block: any, key: string) => {
+    const renderBlocks = () => {
+        return _.map(props.ast.blocks, (block: any, key: string) => {
             return BlockFactory.create(
                 key,
                 block,
                 1,
-                this.props.editorMode,
-                (element) => this.setBlockNode(element, key),
+                props.editorMode,
+                getGridSize(),
             );
         })
     }
 
-    private setBlockNode(element: BlockNode<any> | null, key: string): void {
-        if (element === null) {
-            delete this.blocks[key];
-        } else {
-            this.blocks[key] = element;
-        }
-    }
+    return <div className="node-section" style={style.getStyleMap()}>
+        <GridNode
+            ast={props.ast.grid}
+            editorMode={props.editorMode}
+            gridHeight={getSectionHeight()}
+            children={renderBlocks()}
+            ref={grid}
+        />
+    </div>
 }
