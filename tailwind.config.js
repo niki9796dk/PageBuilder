9e5dfce383c291e014db73a9608fa8023b95a17f
Niki <nikiewald@hotmail.dk>
2022-08-28 11:48:20 +0200

    Correctly installed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,tsx,jsx}"],
+  content: [
+      "index.html",
+      "./src/**/*.{html,js,ts,tsx,jsx}"
+  ],
   theme: {
     extend: {},
   },
